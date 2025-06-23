fa0073d62ba4a8b61098a891f5deed6bc2ad7ec6
Ceeba <yakubceeba@gmail.com>
2025-06-23 14:30:18 +0000

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -78,7 +78,7 @@ const HomePage = () => {
 
   // Featured partners
   const partners = [
-    "Night Market", "Bush Canteen", "Papa's Pizza", "University Bookshop", "Campus Supermarket"
+    "Night Market", "Bush Canteen", "Farmer's Kitchen", "University Bookshop", "All Needs Supermarket"
   ];
 
   return (
